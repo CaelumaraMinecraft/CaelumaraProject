6a9089f88d6f5ca3f06fd280c64088f9314c289b
Attaccer <atacsimum142857@outlook.com>
2025-02-23 10:27:12 +0800

    Add some code

--- KingdomsAddons/Auspice/src/main/java/top/mckingdom/auspice/GroupExt.java
+++ KingdomsAddons/Auspice/src/main/java/top/mckingdom/auspice/GroupExt.java
@@ -1,7 +1,14 @@
 package top.mckingdom.auspice;
 
+import org.kingdoms.constants.player.KingdomPermission;
+
 public final class GroupExt {
 
+    public static final KingdomPermission TEST_PERM = new KingdomPermission(AuspiceAddon.buildNS("TEST_PERM")) {
+        {
+            setHash(50);
+        }
+    };
 
     private GroupExt() {
     }
